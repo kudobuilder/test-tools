adf8d872c5ca39feb5d10d01c0f04f653142ee65
Jan Schlicht <jan@d2iq.com>
2020-02-11 10:34:54 +0100

    Document all public functions

--- pkg/kubernetes/secret.go
+++ pkg/kubernetes/secret.go
@@ -9,30 +9,37 @@ import (
 
 //go:generate go run ../../internal/gen -api CoreV1 -type Secret
 
+// SecretBuilder tracks the options set for a secret.
 type SecretBuilder struct {
 	Name      string
 	Namespace string
 	Data      map[string][]byte
 }
 
+// CreateSecret creates a secret.
+// Additional parameters can be added to this call.
+// The creation is started by calling 'Do'.
 func CreateSecret(name string) SecretBuilder {
 	return SecretBuilder{
 		Name: name,
 	}
 }
 
+// WithNamespace sets the namespace in which the secret will be created.
 func (builder SecretBuilder) WithNamespace(namespace string) SecretBuilder {
 	builder.Namespace = namespace
 
 	return builder
 }
 
+// WithData sets the data the secret should hold.
 func (builder SecretBuilder) WithData(data map[string][]byte) SecretBuilder {
 	builder.Data = data
 
 	return builder
 }
 
+// Do creates the secret in the cluster.
 func (builder SecretBuilder) Do(client client.Client) (Secret, error) {
 	secret := corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
